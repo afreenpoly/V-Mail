0234974ab2e1123f9bcd8ccc6dd2b361c7b54723
aadilsayad <69510854+aadilsayad@users.noreply.github.com>
2024-02-14 23:16:38 +0530

    Changed routes

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -4,6 +4,9 @@ const Main = lazy(() => import('../pages/Main'));
 const Emails = lazy(() => import('../components/Emails'));
 const ViewEmail = lazy(() => import('../components/ViewEmail'));
 const Homepage = lazy(() => import('../components/Homepage/Homepage'));
+const ErrorComponent = lazy(() => import('../components/common/ErrorComponent'))
+const Logout = lazy(() => import('../Logout'))
+const Verify = lazy(() => import('../Verify'))
 
 const routes = {
     main: {
@@ -25,7 +28,16 @@ const routes = {
     homepage: {
         path: '/home',
         element: Homepage
+    },
+    logout: {
+        path: '/logout',
+        element: Logout
+    },
+    verify: {
+        path: '/verify',
+        element: Verify
     }
+
 }
 
 export { routes };
